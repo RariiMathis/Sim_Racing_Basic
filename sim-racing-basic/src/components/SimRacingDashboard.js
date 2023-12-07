dc981af8e8d50e883e3b27c17f5ed6dd48ede786
Rarii Mathis <juniormathis82@gmail.com>
2023-12-07 15:30:48 -0500

    Search bar & Cards

--- sim-racing-basic/src/components/SimRacingDashboard.js
+++ sim-racing-basic/src/components/SimRacingDashboard.js
@@ -14,6 +14,23 @@ const SimRacingDashboard = () => {
     transition: 'background-color 0.3s ease',
   };
 
+  const searchBarStyle = {
+    width: '50%', // Adjust the width as needed
+    padding: '10px',
+    margin: '10px auto',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+  };
+
+  const cardStyle = {
+    padding: '20px',
+    margin: '10px',
+    border: '1px solid #ddd',
+    borderRadius: '4px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    backgroundColor: '#fff',
+  };
+
   return (
     <div>
       <h2>Welcome to the Sim Racing Store!</h2>
@@ -31,6 +48,37 @@ const SimRacingDashboard = () => {
           Wish List
         </Link>
       </nav>
+
+      {/* Search Bar */}
+      <input type="text" placeholder="Search for products" style={searchBarStyle} />
+
+      {/* Cards for different items */}
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div style={cardStyle}>
+          <h3>Wheel Product</h3>
+          <p>Description of the wheel product.</p>
+          <button>Add to Cart</button>
+        </div>
+
+        <div style={cardStyle}>
+          <h3>Pedal Product</h3>
+          <p>Description of the pedal product.</p>
+          <button>Add to Cart</button>
+        </div>
+
+        <div style={cardStyle}>
+          <h3>Cockpit Product</h3>
+          <p>Description of the cockpit product.</p>
+          <button>Add to Cart</button>
+        </div>
+
+        <div style={cardStyle}>
+          <h3>Wishlist Item</h3>
+          <p>Description of the wishlist item.</p>
+          <button>Move to Cart</button>
+        </div>
+      </div>
+
       <hr />
       <Outlet />
     </div>
