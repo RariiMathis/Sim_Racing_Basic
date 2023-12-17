9e19829003674a5fcf797ab42d1ebf4d07a41e18
Rarii Mathis <juniormathis82@gmail.com>
2023-12-17 15:57:58 -0500

    create account front end

--- sim-racing-basic/src/components/SimRacingLogin.js
+++ sim-racing-basic/src/components/SimRacingLogin.js
@@ -4,34 +4,72 @@ import { useNavigate } from 'react-router-dom';
 
 const SimRacingLogin = () => {
   const [driverName, setDriverName] = useState('');
-  const [driverPassword, setdriverPassword] = useState('');
+  const [driverPassword, setDriverPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [isCreatingAccount, setIsCreatingAccount] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Add your sim racing login logic here
-    // For simplicity, let's just check if the driverName and driverPassword are not empty
-    if (driverName && driverPassword) {
-      // Redirect to sim racing dashboard after successful login
-      navigate('/dashboard');
+  const handleToggleMode = () => {
+    setIsCreatingAccount(!isCreatingAccount);
+  };
+
+  const handleAction = () => {
+    if (isCreatingAccount) {
+      // Add your create account logic here
+      if (driverName && driverPassword && confirmPassword === driverPassword && email) {
+        // Redirect to sim racing dashboard after successful account creation
+        navigate('/dashboard');
+      } else {
+        alert('Invalid account information. Please make sure all fields are filled and passwords match.');
+      }
     } else {
-      alert('Invalid credentials. Please enter a valid driver name and team password.');
+      // Add your sim racing login logic here
+      if (driverName && driverPassword) {
+        // Redirect to sim racing dashboard after successful login
+        navigate('/dashboard');
+      } else {
+        alert('Invalid credentials. Please enter a valid driver name and team password.');
+      }
     }
   };
 
   return (
     <div>
-      <h2>Sim Racing Basic</h2>
+      <h2>{isCreatingAccount ? 'Create Account' : 'Sim Racing Basic'}</h2>
       <label>
         Driver's Name:
         <input type="text" value={driverName} onChange={(e) => setDriverName(e.target.value)} />
       </label>
       <br />
+      {isCreatingAccount && (
+        <>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <br />
+        </>
+      )}
       <label>
         Driver's Password:
-        <input type="password" value={driverPassword} onChange={(e) => setdriverPassword(e.target.value)} />
+        <input type="password" value={driverPassword} onChange={(e) => setDriverPassword(e.target.value)} />
       </label>
+      {isCreatingAccount && (
+        <>
+          <br />
+          <label>
+            Confirm Password:
+            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+          </label>
+        </>
+      )}
       <br />
-      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleAction}>{isCreatingAccount ? 'Create Account' : 'Login'}</button>
+      <br />
+      <button onClick={handleToggleMode}>
+        {isCreatingAccount ? 'Already have an account? Login' : 'Create Account'}
+      </button>
     </div>
   );
 };
