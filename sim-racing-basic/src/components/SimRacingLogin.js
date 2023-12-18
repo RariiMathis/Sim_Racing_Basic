7593950d05a0da1c3b62bcf15d498a35e48c3863
Rarii Mathis <juniormathis82@gmail.com>
2023-12-18 11:33:08 -0500

    minor add

--- sim-racing-basic/src/components/SimRacingLogin.js
+++ sim-racing-basic/src/components/SimRacingLogin.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const Card = ({ imgSrc, brand, model, price }) => (
+  <div style={{ border: '1px solid #ddd', padding: '10px', margin: '10px', width: '200px', textAlign: 'center' }}>
+    <img src={imgSrc} alt={`${brand} ${model}`} style={{ width: '100%', height: '150px', objectFit: 'cover', marginBottom: '10px' }} />
+    <p>{brand} {model}</p>
+    <p>${price}</p>
+  </div>
+);
+
 const SimRacingLogin = () => {
   const [driverName, setDriverName] = useState('');
   const [driverPassword, setDriverPassword] = useState('');
@@ -15,17 +23,13 @@ const SimRacingLogin = () => {
 
   const handleAction = () => {
     if (isCreatingAccount) {
-      // Add your create account logic here
       if (driverName && driverPassword && confirmPassword === driverPassword && email) {
-        // Redirect to sim racing dashboard after successful account creation
         navigate('/dashboard');
       } else {
         alert('Invalid account information. Please make sure all fields are filled and passwords match.');
       }
     } else {
-      // Add your sim racing login logic here
       if (driverName && driverPassword) {
-        // Redirect to sim racing dashboard after successful login
         navigate('/dashboard');
       } else {
         alert('Invalid credentials. Please enter a valid driver name and team password.');
@@ -33,9 +37,24 @@ const SimRacingLogin = () => {
     }
   };
 
+  const wheelsData = [
+    { imgSrc: 'https://betanews.com/wp-content/uploads/2020/08/g923-01.png', brand: 'Logitec', model: 'G923', price: 349.99 },
+    // Add more wheel data items as needed
+  ];
+
+  const pedalsData = [
+    { imgSrc: 'https://live.staticflickr.com/8524/29247468925_cd8a6ea8b6_k.jpg', brand: 'Fanatec', model: 'CSL Pedals LC', price: 199.95 },
+    // Add more pedals data items as needed
+  ];
+
+  const cockpitsData = [
+    { imgSrc: 'https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/51ccJG-+OaL._AC_SX569_.jpg', brand: 'Playseat', model: 'Playseat Challenge SimRacing Cockpit', price: 259.99 },
+    // Add more cockpit data items as needed
+  ];
+
   return (
     <div style={{ position: 'relative', height: '100vh' }}>
-      {/* Video in the lower-left corner */}
+      {/* Video component in the lower-left corner */}
       <div style={{ position: 'absolute', left: 0, bottom: -40, width: '200px', height: '325px', marginBottom: '20px' }}>
         <iframe
           width="300%"
@@ -48,20 +67,13 @@ const SimRacingLogin = () => {
         ></iframe>
       </div>
 
-      {/* Images in the lower-right corner */}
+      {/* Image component in the lower-right corner */}
       <div style={{ position: 'absolute', right: -40, bottom: 0, width: '400px', height: '325px', marginBottom: '20px' }}>
-        {/* Image 1 */}
         <img
           src="https://s100.iracing.com/wp-content/uploads/2020/12/EordlGCWMAAOFOL.jpeg"  
           alt="Image 1"
           style={{ position: 'absolute', right: -310, bottom: -70, width: '175%', height: '100%', objectFit: 'cover', marginBottom: '25px' }}
         />
-        {/* Image 2 */}
-        {/* <img
-          src="https://placekitten.com/300/301"  
-          alt="Image 2"
-          style={{ width: '100%', height: '50%', objectFit: 'cover' }}
-        /> */}
       </div>
 
       {/* Content in the center */}
@@ -71,6 +83,28 @@ const SimRacingLogin = () => {
 
         <h1>Your one stop to start your Sim Racing Car</h1>
 
+        {/* Display Wheels, Pedals, and Cockpits cards */}
+        <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+          {/* Display Wheel cards */}
+          {wheelsData.slice(0, 5).map((wheel, index) => (
+            <Card key={`wheel-${index}`} {...wheel} />
+          ))}
+
+          {/* Display Pedals cards */}
+          {pedalsData.slice(0, 5).map((pedal, index) => (
+            <Card key={`pedal-${index}`} {...pedal} />
+          ))}
+
+          {/* Display Cockpit cards */}
+          {cockpitsData.slice(0, 5).map((cockpit, index) => (
+            <Card key={`cockpit-${index}`} {...cockpit} />
+          ))}
+        </div>
+
+        <p>Welcome to the world of simulation racing this page was made to help those who are new.</p>
+        <p>When I began my jouney I wish there was a site like this to help guild me on my purchases</p>
+        <p>and not wasting money on things that things that did not add to the experience.</p>
+        <p>With that all said let's start your career</p>
         <h2>{isCreatingAccount ? 'Create Account' : 'Sim Racing Basic'}</h2>
         <label style={{ marginBottom: '20px' }}>
           Driver's Name:
@@ -110,5 +144,7 @@ const SimRacingLogin = () => {
 };
 
 export default SimRacingLogin;
+
+
 
 
