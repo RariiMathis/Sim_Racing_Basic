9ebf8460996eefc43e1d684f0fe01fd13f2f0db5
Rarii Mathis <juniormathis82@gmail.com>
2023-12-18 01:19:23 -0500

    minor add

--- sim-racing-basic/src/components/SimRacingDashboard.js
+++ sim-racing-basic/src/components/SimRacingDashboard.js
@@ -4,9 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import Card from './Card';
 import Wishlist from './WishList';
 
-
 const SimRacingDashboard = () => {
-  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState('welcome'); // 'welcome' is the default tab
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
@@ -14,7 +13,7 @@ const SimRacingDashboard = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (selectedCategory) {
+      if (selectedCategory !== 'welcome') {
         try {
           setLoading(true);
 
@@ -68,7 +67,6 @@ const SimRacingDashboard = () => {
     setSearchTerm(event.target.value);
   };
 
-  // Filter products based on the search term
   const filteredProducts = products.filter((product) =>
     product.brand.toLowerCase().includes(searchTerm.toLowerCase())
   );
@@ -79,59 +77,70 @@ const SimRacingDashboard = () => {
       <img src="https://traxion.gg/wp-content/uploads/2022/05/cars.jpg" alt="Race Car" style={{ width: '100%', maxHeight: '250px', objectFit: 'cover', marginBottom: '5px' }} />
 
       <h2>Welcome to the Sim Racing Store!</h2>
-      <div style={{ marginBottom: '20px', borderBottom: '1px solid #ccc', display: 'flex', justifyContent: 'center' }}>
-  {['wheels', 'pedals', 'cockpits', 'wishlist'].map((category) => (
-    <button
-      key={category}
-      onClick={() => handleCategorySelect(category)}
-      className={selectedCategory === category ? 'active-tab' : ''}
-      style={{
-        width: '300px',
-        height: '75px',
-        backgroundColor: getCategoryColor(category),
-        color: '#fff',
-        marginRight: '50px', // Adjust the right margin as needed
-      }}
-    >
-      {capitalizeFirstLetter(category)}
-    </button>
-  ))}
-  <button onClick={handleLogout} style={{ width: '80px', height: '40px', backgroundColor: '#34495e', color: '#fff' }}>
-    Logout
-  </button>
-</div>
+
+      {/* Tab Container */}
+      <div style={{ borderBottom: '1px solid #ccc', display: 'flex', justifyContent: 'center' }}>
+        {['welcome', 'wheels', 'pedals', 'cockpits', 'wishlist'].map((category) => (
+          <button
+            key={category}
+            onClick={() => handleCategorySelect(category)}
+            className={selectedCategory === category ? 'active-tab' : ''}
+            style={{
+              width: '150px',
+              height: '50px',
+              backgroundColor: getCategoryColor(category),
+              color: '#fff',
+              margin: '0 10px',
+            }}
+          >
+            {capitalizeFirstLetter(category)}
+          </button>
+        ))}
+        <button onClick={handleLogout} style={{ width: '80px', height: '40px', backgroundColor: '#34495e', color: '#fff', marginLeft: '10px' }}>
+          Logout
+        </button>
+      </div>
 
       {/* Search Bar */}
       <input
-  type="text"
-  placeholder="Search for products"
-  value={searchTerm}
-  onChange={handleSearch}
-  style={{ width: '300px', marginBottom: '25px' }} // Adjust the width as needed
-/>
+        type="text"
+        placeholder="Search for products"
+        value={searchTerm}
+        onChange={handleSearch}
+        style={{ width: '300px', marginBottom: '5px' }}
+      />
 
       {/* Render the selected category component */}
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-        {filteredProducts.map((item, index) => (
-          <Card
-            key={index}
-            item={item}
-            onAddToWishlist={handleAddToWishlist}
-            onDeleteFromWishlist={handleDeleteFromWishlist}
-            isInWishlist={selectedCategory === 'wishlist'}
-          />
-        ))}
-      </div>
-
-      {/* Render Wishlist component at the bottom */}
-      {selectedCategory === 'wishlist' && (
-        <Wishlist />
+      {selectedCategory === 'welcome' ? (
+        <div>
+          <p>
+            This application is made to help everyone starting out into Sim Racing to learn what equipment to buy and the path to the right upgrade.
+          </p>
+          <p>Here are some of the equipment to buy at every price level.</p>
+          <p>Please take a look at what we have in our store.</p>
+        </div>
+      ) : (
+        <div>
+          <h3>Products in the {capitalizeFirstLetter(selectedCategory)} category:</h3>
+          <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+            {filteredProducts.map((item, index) => (
+              <Card
+                key={index}
+                item={item}
+                onAddToWishlist={handleAddToWishlist}
+                onDeleteFromWishlist={handleDeleteFromWishlist}
+                isInWishlist={selectedCategory === 'wishlist'}
+              />
+            ))}
+          </div>
+        </div>
       )}
 
+      {/* Render Wishlist component at the bottom */}
+      {selectedCategory === 'wishlist' && <Wishlist />}
     </div>
   );
-};
-
+}
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -141,15 +150,15 @@ function getCategoryColor(category) {
   switch (category) {
     case 'wheels':
       return '#3498db';
-      case 'pedals':
-        return '#2ecc71';
-        case 'cockpits':
-          return '#e74c3c';
-          case 'wishlist':
-            return '#f39c12';
-            default:
-              return '#fff';
-            }
-          }
-          
-          export default SimRacingDashboard;
+    case 'pedals':
+      return '#2ecc71';
+    case 'cockpits':
+      return '#e74c3c';
+    case 'wishlist':
+      return '#f39c12';
+    default:
+      return '#fff';
+  }
+}
+
+export default SimRacingDashboard;
--- sim-racing-basic/src/components/SimRacingLogin.js
+++ sim-racing-basic/src/components/SimRacingLogin.js
@@ -34,45 +34,81 @@ const SimRacingLogin = () => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      {/* Banner with Race Car Image */}
-      <img src="https://traxion.gg/wp-content/uploads/2022/05/cars.jpg" alt="R8" style={{ width: '100%', maxHeight: '300px', objectFit: 'cover', marginBottom: '20px' }} />
+    <div style={{ position: 'relative', height: '100vh' }}>
+      {/* Video in the lower-left corner */}
+      <div style={{ position: 'absolute', left: 0, bottom: -40, width: '200px', height: '325px', marginBottom: '20px' }}>
+        <iframe
+          width="300%"
+          height="100%"
+          src="https://www.youtube.com/embed/rLfkPntDlO8?si=TE2Gm4vLQSNYaULS"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
 
-      <h1>Your one stop to start your Sim Racing Car</h1>
+      {/* Images in the lower-right corner */}
+      <div style={{ position: 'absolute', right: -40, bottom: 0, width: '400px', height: '325px', marginBottom: '20px' }}>
+        {/* Image 1 */}
+        <img
+          src="https://s100.iracing.com/wp-content/uploads/2020/12/EordlGCWMAAOFOL.jpeg"  
+          alt="Image 1"
+          style={{ position: 'absolute', right: -310, bottom: -70, width: '175%', height: '100%', objectFit: 'cover', marginBottom: '25px' }}
+        />
+        {/* Image 2 */}
+        {/* <img
+          src="https://placekitten.com/300/301"  
+          alt="Image 2"
+          style={{ width: '100%', height: '50%', objectFit: 'cover' }}
+        /> */}
+      </div>
 
-      <h2>{isCreatingAccount ? 'Create Account' : 'Sim Racing Basic'}</h2>
-      <label style={{ marginBottom: '20px' }}>
-        Driver's Name:
-        <input type="text" value={driverName} onChange={(e) => setDriverName(e.target.value)} />
-      </label>
-      {isCreatingAccount && (
-        <>
-          <label style={{ marginBottom: '10px' }}>
-            Email:
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </label>
-        </>
-      )}
-      <label style={{ marginBottom: '30px' }}>
-        Driver's Password:
-        <input type="password" value={driverPassword} onChange={(e) => setDriverPassword(e.target.value)} />
-      </label>
-      {isCreatingAccount && (
-        <>
-          <label style={{ marginBottom: '50px' }}>
-            Confirm Password:
-            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-          </label>
-        </>
-      )}
-      <br />
-      <button onClick={handleAction} style={{ width: '100px', marginBottom: '10px' }}>{isCreatingAccount ? 'Create Account' : 'Login'}</button>
-      <br />
-      <button onClick={handleToggleMode} style={{ marginTop: '5px' }}>
-        {isCreatingAccount ? 'Already have an account? Login' : 'Create Account'}
-      </button>
+      {/* Content in the center */}
+      <div style={{ marginLeft: '320px', marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {/* Banner with Race Car Image */}
+        <img src="https://traxion.gg/wp-content/uploads/2022/05/cars.jpg" alt="R8" style={{ width: '145%', maxHeight: '300px', objectFit: 'cover', marginBottom: '20px' }} />
+
+        <h1>Your one stop to start your Sim Racing Car</h1>
+
+        <h2>{isCreatingAccount ? 'Create Account' : 'Sim Racing Basic'}</h2>
+        <label style={{ marginBottom: '20px' }}>
+          Driver's Name:
+          <input type="text" value={driverName} onChange={(e) => setDriverName(e.target.value)} />
+        </label>
+        {isCreatingAccount && (
+          <>
+            <label style={{ marginBottom: '10px' }}>
+              Email:
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+          </>
+        )}
+        <label style={{ marginBottom: '30px' }}>
+          Driver's Password:
+          <input type="password" value={driverPassword} onChange={(e) => setDriverPassword(e.target.value)} />
+        </label>
+        {isCreatingAccount && (
+          <>
+            <label style={{ marginBottom: '50px' }}>
+              Confirm Password:
+              <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+            </label>
+          </>
+        )}
+        <br />
+        <button onClick={handleAction} style={{ width: '100px', marginBottom: '10px' }}>
+          {isCreatingAccount ? 'Create Account' : 'Login'}
+        </button>
+        <br />
+        <button onClick={handleToggleMode} style={{ marginTop: '5px' }}>
+          {isCreatingAccount ? 'Already have an account? Login' : 'Create Account'}
+        </button>
+      </div>
     </div>
   );
 };
 
 export default SimRacingLogin;
+
+
